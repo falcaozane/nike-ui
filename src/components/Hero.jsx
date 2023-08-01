e9df7952f4f34c08d900a8fc100d3be71e2358f4
Zane Falcao <112317470+falcaozane@users.noreply.github.com>
2023-08-02 00:05:31 +0530

    deploy

---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+
+
+const Hero = () => {
+  return (
+    <main className="hero flex max-w-7xl mx-auto mt-5 items-center">
+        <div className="hero-content items-center ml-0 mr-auto">
+            <h1 className="text-7xl font-extrabold max-w-lg pr-10 mb-10">YOUR FEET DESERVE THE BEST</h1>
+            <p className="font-semibold text-slate-500 max-w-sm mb-5">YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+               YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+            </p>
+            <div className="hero-btn grid lg:grid-cols-2 gap-4 max-w-sm mb-5">
+                <button className="bg-red-600 text-white border-2 border-red-600 rounded-full p-2 hover:bg-white hover:text-red-600">Shop Now</button>
+                <button className=" border-2 border-red-600 text-red-500 rounded-full p-2 hover:bg-red-600 hover:text-white">Category</button>
+            </div>
+            <div className="brand-icons">
+                <p className="my-2">Available on:</p>
+                <div className="flex my-2 gap-6">
+                    <img src="images/flipkart.png" alt="flipkart" className="cursor-pointer " />
+                    <img src="images/amazon.png" alt="amazon" className="cursor-pointer" />
+                </div>
+            </div>
+        </div>
+            <div className="hero-image">
+                <img src="images/hero-image.png" alt="product-image"  />
+            </div>
+    </main>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+
+
+export const Navbar = () => {
+  return (
+    <div className='mx-auto my-4 items-center'>
+        <nav className='flex justify-between h-[72px] max-w-7xl mx-auto'>
+            <div className='logo mt-2'>
+                <img src='images/brand_logo.png' alt='Nike'/>
+            </div>
+            <div className='flex'>
+                <ul className='flex gap-6 items-baseline mt-5 font-semibold'>
+                    <li href="" className="hover:underline">Menu</li>
+                    <li href="" className="hover:underline">Location</li>
+                    <li href="" className="hover:underline">About</li>
+                    <li href="" className="hover:underline">Contact</li>
+                </ul>
+            </div>
+            <button className="rounded-full bg-red-600 text-white px-3 h-[34px] w-[72px] mt-4 place-items-center text-xl">Login</button>
+        </nav>
+    </div>
+  )
+}
